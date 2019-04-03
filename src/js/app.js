e08348bea52b7bb2cb6b3e8b458c00e58df031ed
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2019-04-03 00:14:43 -0500

    Modified css

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 /* sweetScroll load */
 document.addEventListener("DOMContentLoaded", function () {
-  new SweetScroll({/* some options */ });
+  new SweetScroll({
+    /* some options */
+  });
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS('particles-js', {
